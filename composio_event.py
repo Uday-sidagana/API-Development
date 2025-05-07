a23da40c16f56a341c0603409a2a7edf6d628bca
Uday-sidagana <uday.sidgana@gmail.com>
2025-05-07 22:03:39 +0530

    Tried thhe general approach

--- composio_event.py
+++ composio_event.py
@@ -13,12 +13,14 @@ prompt = hub.pull("hwchase17/openai-functions-agent")
 
 
 composio_toolset = ComposioToolSet(api_key= os.getenv("COMPSIO_API_KEY"))
-tools = composio_toolset.get_tools(actions=['GOOGLECALENDAR_CREATE_EVENT'])
+# tools = composio_toolset.get_tools(actions=['GOOGLECALENDAR_CREATE_EVENT'])
+tools = composio_toolset.get_tools(apps=[App.GOOGLECALENDAR])
 # prompt = hub.pull("hwchase17/openai-functions-agent")
 
 agent = create_openai_functions_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-task = "Create an event on Wed 7th May 2025 10PM. Title is 'Ai Agent Test' and Description is 'Test"
+# task = "Create an event on Wed 7th May 2025 11PM. Title is 'Ai Agent Test' and Description is 'Test"
+task = "Give me details of Events scheduled today, just give me title and time"
 result = agent_executor.invoke({"input": task})
 print(result)
