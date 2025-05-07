ac273edcbd5e20d6ab17cb91e57ce1733630f1ba
Uday-sidagana <uday.sidgana@gmail.com>
2025-05-07 11:46:43 +0530

    Getting username from my github username

--- composio.py
+++ composio.py
@@ -6,3 +6,28 @@ from openai import OpenAI
 # Uses the 'default' entity_id if not specified
 toolset = ComposioToolSet()
 client = OpenAI()
+
+#Composio Tools
+tools = toolset.get_tools(actions=[Action.GITHUB_GET_THE_AUTHENTICATED_USER])
+
+#OpenAI workflow
+task = "Get my Github Username"
+
+message = [
+    {'role': 'system', 'content': 'You are expert in using tools'},
+    {'role': 'client', 'content': task},
+]
+
+response = client.chat.completions.create(
+    model='chatgpt-4o-latest',
+    messages= message,
+    tools=tools,
+    tool_choice= 'auto',
+
+)
+
+
+#Use ComposioToolset to handle calls
+result = toolset.handle_tool_calls(response)
+
+print(result)
